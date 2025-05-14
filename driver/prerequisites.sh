aba3dfce0ab6090e62bf15ef2a09cea6ae6f9679
Lukas Johannes Breitwieser <lukas.breitwieser@gmail.com>
2025-05-14 18:01:54 +0200

    Check if spack is available

--- driver/prerequisites.sh
+++ driver/prerequisites.sh
@@ -8,5 +8,12 @@ cd "$SCRIPT_DIR"
 . util.sh
 cd "$BUILD_DIR"
 
+if [ command spack &>/dev/null ]; then
+  echo "ERROR spack was not found"
+  echo "Please make sure spack is installed on your system, added to the PATH,"
+  echo "and has the shell extension sourced"
+  echo "Aborting..."
+fi
+
 spack install hpctoolkit
 
